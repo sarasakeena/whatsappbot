d2ab5c98ede776a6c0d88e0596f3accd3693cece
sarasakeena <sarasakeena@gmail.com>
2025-06-16 19:31:01 +0530

    Update gitignore and config files securely

--- encode.py
+++ encode.py
@@ -1,6 +1,6 @@
 import base64
 
-with open("whatsappbot-463107-0fea5ed4b30e.json", "rb") as f:
+with open("whatsappbot-463107-c6d0e1e6f9d0.json", "rb") as f:
     encoded = base64.b64encode(f.read()).decode()
 
 print(encoded)
